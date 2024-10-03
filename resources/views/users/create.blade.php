493f012786a18b6fd2612340b269f65b9c1f045d
pia <jacquelinesophiatoquero@gmail.com>
2024-10-03 16:53:34 +0800

    bago
    bago

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Create Product')
  
 @section('contents')
-<h1 class="font-bold text-2xl ml-3">Add Product</h1>
+<h1 class="font-bold text-2xl ml-3">Add User</h1>
 <hr />
 <div class="border-b border-gray-900/10 pb-12">
     <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -19,7 +19,7 @@
             <div class="sm:col-span-4">
                 <label class="block text-sm font-medium leading-6 text-gray-900">Email</label>
                 <div class="mt-2">
-                    <input id="email" name="email" type="text" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <input id="email" name="email" type="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
             <div class="sm:col-span-4">
